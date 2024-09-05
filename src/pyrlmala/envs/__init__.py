86a7eedbd3e05f1458d7e4495373f0cb35217d88
Congye Wang <congye.wang@outlook.com>
2024-09-05 22:12:46 +0100

    Change type of env

--- src/pyrlmala/envs/__init__.py
+++ src/pyrlmala/envs/__init__.py
@@ -1,17 +1,10 @@
-from typing import TypeAlias
-
-import gymnasium as gym
-import numpy as np
-import numpy.typing as npt
 from gymnasium.envs.registration import register
 
 from ._env import RLMALAEnv
-
-RLMCMCEnv: TypeAlias = gym.Env[npt.NDArray[np.float64], npt.NDArray[np.float64]]
 
 register(
     id="RLMALAEnv-v1.0",
     entry_point="src.pyrlmala.envs._env:RLMALAEnv",
 )
 
-__all__ = ["RLMALAEnv", "RLMCMCEnv"]
+__all__ = ["RLMALAEnv"]
