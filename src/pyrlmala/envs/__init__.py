161f6f38ebf8d945d70ecdb6ef9dc77b5ca4d5ec
Congye Wang <congye.wang@outlook.com>
2024-11-08 16:19:03 +0000

    Exposing MCMCEnvBase

--- src/pyrlmala/envs/__init__.py
+++ src/pyrlmala/envs/__init__.py
@@ -1,10 +1,10 @@
 from gymnasium.envs.registration import register
 
-from ._env import BarkerEnv
+from ._env import MCMCEnvBase, BarkerEnv
 
 register(
     id="BarkerEnv-v1.0",
     entry_point="src.pyrlmala.envs._env:BarkerEnv",
 )
 
-__all__ = ["BarkerEnv"]
+__all__ = ["MCMCEnvBase", "BarkerEnv"]
