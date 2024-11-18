55d601186e0925507a39f195716daa696ee53109
Congye Wang <congye.wang@outlook.com>
2024-11-18 22:48:20 +0000

    Delete MCMCAnimation

--- src/pyrlmala/learning/_running.py
+++ src/pyrlmala/learning/_running.py
@@ -8,7 +8,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-from ..utils import MCMCAnimation, Toolbox
+from ..utils import Toolbox
 from . import LearningFactory
 
 from typing import Union, Callable
