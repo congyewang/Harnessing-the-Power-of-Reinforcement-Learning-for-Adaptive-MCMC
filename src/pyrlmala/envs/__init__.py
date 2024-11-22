b7a4c29334ef0b12286c4b78533aba24165af16a
Congye Wang <congye.wang@outlook.com>
2024-11-22 21:07:32 +0000

    Bug fixed of entry_point

--- src/pyrlmala/envs/__init__.py
+++ src/pyrlmala/envs/__init__.py
@@ -1,10 +1,10 @@
 from gymnasium.envs.registration import register
 
-from .env import MCMCEnvBase, BarkerEnv
+from .env import BarkerEnv, MCMCEnvBase
 
 register(
     id="BarkerEnv-v1.0",
-    entry_point="src.pyrlmala.envs._env:BarkerEnv",
+    entry_point="src.pyrlmala.envs.env:BarkerEnv",
 )
 
 __all__ = ["MCMCEnvBase", "BarkerEnv"]
