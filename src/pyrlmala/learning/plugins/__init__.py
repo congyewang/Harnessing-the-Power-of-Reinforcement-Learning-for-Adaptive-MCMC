329954572ac4e6509b6cc8acf92fedcd4de310c2
Congye Wang <congye.wang@outlook.com>
2025-03-17 20:44:56 +0000

    feat(plugins): expose PluginBase in the module's __all__ for better accessibility

--- src/pyrlmala/learning/plugins/__init__.py
+++ src/pyrlmala/learning/plugins/__init__.py
@@ -1,9 +1,11 @@
+from .base import PluginBase
 from .configger import ActorLearningRateConfig, CriticLearningRateConfig
 from .plotter import TrainingVisualizer
 from .saver import ActorSaver, CriticSaver
 from .slider import ActorLearningRateSlider
 
 __all__ = [
+    "PluginBase",
     "ActorLearningRateConfig",
     "CriticLearningRateConfig",
     "TrainingVisualizer",
