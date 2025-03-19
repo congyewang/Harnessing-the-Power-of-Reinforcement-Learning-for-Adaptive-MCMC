f4edda2959f17cfd88cd37d0e7ff46217fff8b9f
Congye Wang <congye.wang@outlook.com>
2025-03-19 11:48:11 +0000

    feat(experiments): remove StepLR scheduler configuration and add new result files for Laplace experiments

---
+++ experiments/laplace_4/flex_run.py
@@ -0,0 +1,25 @@
+from pyrlmala.utils.flex import FlexibleBatchRunner
+
+
+if __name__ == "__main__":
+    flex_batch_run = FlexibleBatchRunner(
+        model_name="test-laplace_4-test-laplace_4",
+        posteriordb_path="../posteriordb/posterior_database",
+        load_policy="best",
+    )
+    # mcmc_env_list = ["mala", "mala_esjd", "barker", "barker_esjd"]
+    mcmc_env_list = ["barker_esjd"]
+
+    for i in mcmc_env_list:
+        flex_batch_run.run(
+            mcmc_env=i,
+            step_size=0.1,
+            repeat_count=10,
+            save_root_path=".",
+            template_path="./config/config_template.toml",
+            output_root_path="./config",
+        )
+
+    import requests
+
+    requests.post("https://ntfy.greenlimes.top/asus", data="Finished")
