c003e7e6e0bbfc00c2b566c9201c30de4b1446ec
Congye Wang <congye.wang@outlook.com>
2025-03-24 14:51:37 +0000

    feat(utils): remove CalculateMMD from utils exports

--- src/pyrlmala/utils/__init__.py
+++ src/pyrlmala/utils/__init__.py
@@ -1,13 +1,11 @@
 from .utils import (
     AveragePolicy,
-    CalculateMMD,
     NUTSFromPosteriorDB,
     Toolbox,
 )
 
 __all__ = [
     "AveragePolicy",
-    "CalculateMMD",
     "NUTSFromPosteriorDB",
     "Toolbox",
 ]
