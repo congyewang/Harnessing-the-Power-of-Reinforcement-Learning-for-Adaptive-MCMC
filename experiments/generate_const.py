bfdc2735069c5f6a515e7aa7e47310b3abea412a
Congye Wang <congye.wang@outlook.com>
2025-04-05 14:00:13 +0100

    Simplifies experiment script generation
    Removes unnecessary file aggregation and sbatch submission logic from the experiment generation script. This change streamlines the process and reduces complexity. The number of CPUs requested in the bash script template has also been removed.

--- experiments/generate_const.py
+++ experiments/generate_const.py
@@ -9,8 +9,6 @@ step_size_list = list(range(1, 20, 1)) + [0.1, 0.5]
 
 py_template_path = "./template/template.const_run.py"
 bash_template_path = "./template/template.run-const.sh"
-sbatch_path_list = []
-
 
 with open(py_template_path, "r") as file:
     py_template = jinja2.Template(file.read())
@@ -35,12 +33,3 @@ for idx, model_name in enumerate(model_name_list):
         bash_output_path = f"./{dir_name[idx]}/const/run-const_{step_size}.sh"
         with open(bash_output_path, "w") as file:
             file.write(bash_template.render(context))
-
-        sbatch_path_list.append(bash_output_path)
-
-
-with open("./run-total.sh", "w") as f:
-    f.write("#!/bin/bash\n")
-
-    for i in sbatch_path_list:
-        f.write(f"sbatch {i}\n")
