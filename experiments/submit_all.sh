c466c9d3ab108aa098aa2ff01d8a130fa77d7e7b
Congye Wang <congye.wang@outlook.com>
2025-04-05 14:22:37 +0100

    Adds script to submit all experiment jobs
    Adds a script to submit all experiment jobs for different laplace configurations.
    
    This simplifies the process of running all experiments by automating the submission of sbatch scripts.

---
+++ experiments/submit_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd laplace_1/const
+for file in *.sh; do
+    sbatch "$file"
+done
+cd -
+
+cd laplace_2/const
+for file in *.sh; do
+    sbatch "$file"
+done
+cd -
+
+cd laplace_1/const
+for file in *.sh; do
+    sbatch "$file"
+done
