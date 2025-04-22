0e290296930a9dd611872058e21ed7ab0df57263
Congye Wang <congye.wang@outlook.com>
2025-04-22 13:32:10 +0100

    feat(hyperparameters): Add scripts and templates for hyperparameter search configuration

---
+++ experiments/search_hyperparameters/templete/run-search.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=flex-{{ critic_learning_rate }}_{{ actor_learning_rate }}_{{ tau }}
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-type=FAIL
+
+module load GCC
+
+source /mnt/nfs/home/c2029946/Code/PythonProjects/pyrlmala/.venv/bin/activate
+
+python run_search_{{ critic_learning_rate }}_{{ actor_learning_rate }}_{{ tau }}.py
+
+curl -d "✅ SLURM Job ${SLURM_JOB_NAME} Finished Successfully" https://ntfy.greenlimes.top/asus
+
+echo "Finished Job"
+exit 0
