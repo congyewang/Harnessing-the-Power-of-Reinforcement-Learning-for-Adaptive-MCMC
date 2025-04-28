0a5bb3b3dc9b0198b1710f1c4975dd6d8b5a0af0
Congye Wang <congye.wang@outlook.com>
2025-04-28 02:21:16 +0100

    feat: Refactor MMD calculation into a separate function and update parameters for consistency

--- experiments/baseline/templete/baseline_run.py
+++ experiments/baseline/templete/baseline_run.py
@@ -1,6 +1,7 @@
 import numpy as np
 from mcmclib.metropolis import mala_adapt
 
+from pyrlmala.envs import MALAEnv
 from pyrlmala.utils import Toolbox
 from pyrlmala.utils.target import AutoStanTargetPDF
 
@@ -20,18 +21,47 @@ fp, fg = target.combine_make_log_target_pdf(["pdf", "grad"])
 gs = Toolbox.gold_standard(model_name, posteriordb_path)
 
 
-for random_seed in range(replicate_num):
-    np.random.seed(random_seed)
-    res = mala_adapt(
+def run_mmd(random_seed: int) -> float:
+    const_mala = mala_adapt(
         fp=fp,
         fg=fg,
         x0=gs[0],
         h0=0.1,
         c0=np.cov(gs, rowvar=False),
-        alpha=[0.3] * 10 + [0.0],
-        epoch=[5_000] * 10 + [10_000],
+        alpha=[1.0] * 101,
+        epoch=[500] * 100 + [10_000],
     )
-    mmd = Toolbox.calculate_mmd(gs, res[0][-1], Toolbox.median_trick(gs))
+
+    step_size = const_mala[-2] ** 2
+
+    baseline_env = MALAEnv(
+        log_target_pdf_unsafe=fp,
+        grad_log_target_pdf_unsafe=fg,
+        initial_sample=gs[0],
+        initial_covariance=np.cov(gs, rowvar=False),
+        initial_step_size=step_size,
+        total_timesteps=10_000,
+        max_steps_per_episode=500,
+        log_mode=True,
+    )
+
+    action = Toolbox.softplus(np.tile(step_size, 2))
+    baseline_env.reset(seed=random_seed)
+
+    for _ in range(10_000):
+        baseline_env.step(action)
+
+    mmd = Toolbox.calculate_mmd(
+        gs, baseline_env.store_accepted_sample, Toolbox.median_trick(gs)
+    )
+
+    return mmd
+
+
+for random_seed in range(replicate_num):
+    np.random.seed(random_seed)
+    mmd = run_mmd(random_seed)
+    print(f"Random seed: {random_seed}, MMD: {mmd}")
 
     with open(mmd_file_path, "a+") as f:
         f.write(f"{model_name},{random_seed},{mmd}\n")
