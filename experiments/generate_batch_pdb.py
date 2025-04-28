cd0511989e28c7326e017f40d927d8e160a6a8fe
Congye Wang <congye.wang@outlook.com>
2025-04-28 13:14:04 +0100

    feat: Update initial step size logic and modify actor configuration for improved training dynamics

--- experiments/generate_batch_pdb.py
+++ experiments/generate_batch_pdb.py
@@ -4,7 +4,6 @@ from itertools import product
 from typing import Dict
 
 import jinja2
-import numpy as np
 
 from pyrlmala.utils import Toolbox
 from pyrlmala.utils.posteriordb import PosteriorDBToolbox
@@ -31,28 +30,114 @@ def create_model_result_dir(model_name: str) -> str:
     return model_result_dir
 
 
-def output_initial_step_size(model_sample_dim: int) -> float:
+def output_initial_step_size(model_name: str) -> float:
     """
-    Output the initial step size for the model.
+    Output a smooth initial step size based on model dimension using log-linear interpolation.
 
     Args:
-        model_sample_dim (int): The dimension of the model sample.
+        model_name (str): The name of the model.
 
     Returns:
-        float: The initial step size.
+        float: The initial step size for the model.
     """
-    if model_sample_dim <= 10:
-        return 0.5
-    elif model_sample_dim >= 100:
-        return 0.01
+    if model_name == "kidiq_with_mom_work-kidscore_mom_work":
+        step_size = 2.96e-02
+    elif model_name == "bball_drive_event_0-hmm_drive_0":
+        step_size = 3.36e-02
+    elif model_name == "kilpisjarvi_mod-kilpisjarvi":
+        step_size = 3.75e-02
+    elif model_name == "kidiq-kidscore_momhsiq":
+        step_size = 6.66e-03
+    elif model_name == "eight_schools-eight_schools_noncentered":
+        step_size = 5.64e-01
+    elif model_name == "nes2000-nes":
+        step_size = 8.59e-04
+    elif model_name == "nes1996-nes":
+        step_size = 2.91e-06
+    elif model_name == "earnings-logearn_interaction":
+        step_size = 3.06e-02
+    elif model_name == "arK-arK":
+        step_size = 4.37e-05
+    elif model_name == "gp_pois_regr-gp_regr":
+        step_size = 8.78e-02
+    elif model_name == "diamonds-diamonds":
+        step_size = 1.02e-07
+    elif model_name == "garch-garch11":
+        step_size = 2.00e-03
+    elif model_name == "mesquite-logmesquite":
+        step_size = 3.29e-04
+    elif model_name == "mesquite-logmesquite_logva":
+        step_size = 2.15e-02
+    elif model_name == "nes1992-nes":
+        step_size = 1.08e-07
+    elif model_name == "nes1976-nes":
+        step_size = 9.61e-08
+    elif model_name == "sblri-blr":
+        step_size = 1.41e-02
+    elif model_name == "earnings-log10earn_height":
+        step_size = 7.81e-02
+    elif model_name == "earnings-logearn_height_male":
+        step_size = 7.74e-03
+    elif model_name == "nes1988-nes":
+        step_size = 1.04e-07
+    elif model_name == "mesquite-logmesquite_logvas":
+        step_size = 2.56e-04
+    elif model_name == "earnings-earn_height":
+        step_size = 1.70e-05
+    elif model_name == "mcycle_gp-accel_gp":
+        step_size = 5.64e-01
+    elif model_name == "hmm_example-hmm_example":
+        step_size = 4.26e-03
+    elif model_name == "bball_drive_event_1-hmm_drive_1":
+        step_size = 1.38e-02
+    elif model_name == "nes1980-nes":
+        step_size = 8.29e-04
+    elif model_name == "earnings-logearn_height":
+        step_size = 7.24e-02
+    elif model_name == "one_comp_mm_elim_abs-one_comp_mm_elim_abs":
+        step_size = 1.37e-06
+    elif model_name == "kidiq-kidscore_momhs":
+        step_size = 8.16e-02
+    elif model_name == "earnings-logearn_logheight_male":
+        step_size = 2.67e-02
+    elif model_name == "kidiq-kidscore_momiq":
+        step_size = 1.71e-01
+    elif model_name == "hudson_lynx_hare-lotka_volterra":
+        step_size = 1.93e-04
+    elif model_name == "nes1972-nes":
+        step_size = 1.02e-07
+    elif model_name == "sblrc-blr":
+        step_size = 2.68e-05
+    elif model_name == "kidiq_with_mom_work-kidscore_interaction_c":
+        step_size = 3.08e-02
+    elif model_name == "nes1984-nes":
+        step_size = 8.99e-08
+    elif model_name == "eight_schools-eight_schools_centered":
+        step_size = 6.33e-08
+    elif model_name == "earnings-logearn_interaction_z":
+        step_size = 3.18e-02
+    elif model_name == "mesquite-logmesquite_logvash":
+        step_size = 1.91e-05
+    elif model_name == "mesquite-logmesquite_logvolume":
+        step_size = 4.36e-02
+    elif model_name == "kidiq-kidscore_interaction":
+        step_size = 3.07e-02
+    elif model_name == "arma-arma11":
+        step_size = 5.06e-03
+    elif model_name == "kidiq_with_mom_work-kidscore_interaction_c2":
+        step_size = 1.24e-03
+    elif model_name == "kidiq_with_mom_work-kidscore_interaction_z":
+        step_size = 3.05e-02
+    elif model_name == "low_dim_gauss_mix-low_dim_gauss_mix":
+        step_size = 2.95e-02
+    elif model_name == "gp_pois_regr-gp_pois_regr":
+        step_size = 5.64e-01
+    elif model_name == "mesquite-mesquite":
+        step_size = 3.28e-04
     else:
-        # Log-linear interpolate between 0.5 (dim=10) and 0.01 (dim=100)
-        log_eps = np.interp(
-            np.log(model_sample_dim),
-            [np.log(10), np.log(100)],
-            [np.log(0.5), np.log(0.01)],
-        )
-        return float(np.exp(log_eps))
+        step_size = 1.0e-2
+
+    return step_size
 
 
 def generate_files(
@@ -94,6 +179,7 @@ def generate_files(
                 "random_seed": str(random_seed),
                 "env_id": mcmc_env_dict[mcmc_env],
                 "actor_learning_rate": "1e-5",
+                "exploration_noise": str(0.2 * output_initial_step_size(model_name)),
             }
             hyperparameter_template_path = (
                 f"{template_root_dir}/config_template_pdb.toml"
@@ -139,9 +225,7 @@ def generate_files(
         with open(bash_template_path, "r") as file:
             bash_template = jinja2.Template(file.read())
         bash_content = bash_template.render(bash_context)
-        bash_script_path = (
-            f"{model_result_dir}/run_bash_{rl_algorithm}_{mcmc_env}.sh"
-        )
+        bash_script_path = f"{model_result_dir}/run_bash_{rl_algorithm}_{mcmc_env}.sh"
         with open(bash_script_path, "w") as file:
             file.write(bash_content)
 
