50fa8381994e75cf0a3cdafddce32ee3ae8dbb2e
Congye Wang <congye.wang@outlook.com>
2025-05-08 22:28:19 +0800

    feat: Update step size in flex_batch_run to improve sampling efficiency

--- experiments/laplace_2/flex_run.py
+++ experiments/laplace_2/flex_run.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     for i in mcmc_env_list:
         flex_batch_run.run(
             mcmc_env=i,
-            step_size=2.0,
+            step_size=2.5,
             initial_sample=0.1 * np.ones(2),
             repeat_count=10,
             save_root_path=".",
