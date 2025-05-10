7f629a4151d0764cb79c6f1100027d94481bcd49
Congye Wang <congye.wang@outlook.com>
2025-05-10 20:12:50 +0800

    feat: Reduce total timesteps and predicted timesteps for improved training efficiency

--- experiments/baseline/templete/baseline_run.py
+++ experiments/baseline/templete/baseline_run.py
@@ -41,7 +41,7 @@ def run_mmd(random_seed: int) -> float:
         initial_sample=gs[0],
         initial_covariance=initial_covariance,
         initial_step_size=step_size,
-        total_timesteps=10_000,
+        total_timesteps=5_000,
         max_steps_per_episode=500,
         log_mode=True,
     )
@@ -49,7 +49,7 @@ def run_mmd(random_seed: int) -> float:
     action = Toolbox.softplus(np.tile(step_size, 2))
     baseline_env.reset(seed=random_seed)
 
-    for _ in range(10_000):
+    for _ in range(5_000):
         baseline_env.step(action)
 
     mmd = Toolbox.calculate_mmd(
