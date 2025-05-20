f56144f067e87c8062d57337293c1ba0e0e6046b
Congye Wang <congye.wang@outlook.com>
2025-05-20 17:39:18 +0800

    feat: Add generation scripts and templates for baseline Barker experiment

---
+++ experiments/baseline_barker/templete/run-baseline_barker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=baseline-barker-{{ model_name }}
+#SBATCH --output=slurm-%j.out
+#SBATCH --error=slurm-%j.err
+#SBATCH --time=2-00:00:00
+#SBATCH --mail-type=FAIL
+
+module load GCC
+
+source /mnt/nfs/home/c2029946/Code/PythonProjects/pyrlmala/.venv/bin/activate
+
+python baseline_barker_run_{{ model_name }}.py
+
+curl -d "✅ SLURM Job ${SLURM_JOB_NAME} Finished Successfully" https://ntfy.greenlimes.top/asus
+
+echo "Finished Job"
+exit 0
