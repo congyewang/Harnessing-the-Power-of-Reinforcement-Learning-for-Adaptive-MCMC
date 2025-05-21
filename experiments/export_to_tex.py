e261f82582b2fddd0100bd9cae40bc2f7233c4fb
Congye Wang <congye.wang@outlook.com>
2025-05-21 19:34:37 +0800

    feat: Update get_dataframe to include sensitivity analysis results and enhance output_tex for sensitive data

--- experiments/export_to_tex.py
+++ experiments/export_to_tex.py
@@ -7,19 +7,21 @@ from pyrlmala.utils import Toolbox
 from pyrlmala.utils.export import (
     EnhancedTableGenerator,
     PosteriorDBGenerator,
+    SingleTableGenerator,
     TableGenerator,
 )
 from pyrlmala.utils.read import BaselineResultReader, MCMCResultReader
 
 
-def get_dataframe() -> Tuple[pd.DataFrame, pd.DataFrame]:
+def get_dataframe() -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
     Generate DataFrames for RMALA and RLBarker results.
 
     Returns:
-        Tuple[pd.DataFrame, pd.DataFrame]: A tuple containing two DataFrames:
-            - The first DataFrame contains the results from RMALA AAR, RMALA ESJD, RMALA-RLMH CDLB, and RMALA-RLMH LESJD.
-            - The second DataFrame contains the results from RLBarker CDLB and RLBarker LESJD.
+        Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]: A tuple containing three DataFrames:
+            - RMALA results DataFrame
+            - RLBarker results DataFrame
+            - Sensitivity analysis results DataFrame
     """
     baseline_aar_reader = BaselineResultReader(
         results_dir="./baseline", method_name="RMALA AAR"
@@ -42,6 +44,9 @@ def get_dataframe() -> Tuple[pd.DataFrame, pd.DataFrame]:
     )
     rl_barker_esjd_reader = MCMCResultReader(
         results_dir="./whole_results_barker_esjd", method_name="RLBarker LESJD"
+    )
+    rl_sensitive_reader = MCMCResultReader(
+        results_dir="./sensitivity", method_name="RMALA-RLMH CDLB"
     )
 
     baseline_aar_exporter = PosteriorDBGenerator(
@@ -66,6 +71,9 @@ def get_dataframe() -> Tuple[pd.DataFrame, pd.DataFrame]:
     rl_barker_esjd_exporter = PosteriorDBGenerator(
         rl_barker_esjd_reader, "./posteriordb/posterior_database"
     )
+    rl_sensitive_exporter = PosteriorDBGenerator(
+        rl_sensitive_reader, "./posteriordb/posterior_database"
+    )
 
     baseline_aar_df = baseline_aar_exporter.get_result_dataframe()
     baseline_esjd_df = baseline_esjd_exporter.get_result_dataframe()
@@ -75,6 +83,7 @@ def get_dataframe() -> Tuple[pd.DataFrame, pd.DataFrame]:
     baseline_barker_df = baseline_barker_exporter.get_result_dataframe()
     rl_barker_cdlb_df = rl_barker_cdlb_exporter.get_result_dataframe()
     rl_barker_esjd_df = rl_barker_esjd_exporter.get_result_dataframe()
+    rl_sensitive_cdlb_df = rl_sensitive_exporter.get_result_dataframe()
 
     merged_df_mala = pd.merge(baseline_aar_df, baseline_esjd_df, on=["Model", "d"])
     merged_df_mala = pd.merge(merged_df_mala, rl_cdlb_df, on=["Model", "d"])
@@ -85,7 +94,7 @@ def get_dataframe() -> Tuple[pd.DataFrame, pd.DataFrame]:
     )
     merged_df_barker = pd.merge(merged_df_barker, rl_barker_esjd_df, on=["Model", "d"])
 
-    return merged_df_mala, merged_df_barker
+    return merged_df_mala, merged_df_barker, rl_sensitive_cdlb_df
 
 
 def get_mean_sub_dataframe(df: pd.DataFrame) -> pd.DataFrame:
@@ -122,7 +131,7 @@ def output_tex(mode: str = "mean", output_root_dir: str = ".") -> None:
         mode (str): The mode to use for generating the table. Can be 'mean', 'median', or 'all'.
         output_root_dir (str): The root directory where the output file will be saved.
     """
-    df_mala, df_barker = get_dataframe()
+    df_mala, df_barker, df_sensitive = get_dataframe()
 
     match mode:
         case "mean":
@@ -139,6 +148,15 @@ def output_tex(mode: str = "mean", output_root_dir: str = ".") -> None:
             )
             Toolbox.create_folder(mean_output_file_path_barker)
             EnhancedTableGenerator.output(mean_df_barker, mean_output_file_path_barker)
+
+            mean_df_sensitive = get_mean_sub_dataframe(df_sensitive)
+            mean_output_file_path_sensitive = os.path.join(
+                output_root_dir, "mean_results_sensitive.tex"
+            )
+            Toolbox.create_folder(mean_output_file_path_sensitive)
+            SingleTableGenerator.output(
+                mean_df_sensitive, mean_output_file_path_sensitive
+            )
         case "median":
             median_df_mala = get_median_sub_dataframe(df_mala)
             median_output_file_path_mala = os.path.join(
@@ -154,6 +172,15 @@ def output_tex(mode: str = "mean", output_root_dir: str = ".") -> None:
             Toolbox.create_folder(median_output_file_path_barker)
             EnhancedTableGenerator.output(
                 median_df_barker, median_output_file_path_barker
+            )
+
+            median_df_sensitive = get_median_sub_dataframe(df_sensitive)
+            median_output_file_path_sensitive = os.path.join(
+                output_root_dir, "median_results_sensitive.tex"
+            )
+            Toolbox.create_folder(median_output_file_path_sensitive)
+            SingleTableGenerator.output(
+                median_df_sensitive, median_output_file_path_sensitive
             )
         case "all":
             median_df_mala = get_median_sub_dataframe(df_mala)
@@ -189,6 +216,23 @@ def output_tex(mode: str = "mean", output_root_dir: str = ".") -> None:
             EnhancedTableGenerator.output(
                 median_df_barker, median_output_file_path_barker
             )
+
+            median_df_sensitive = get_median_sub_dataframe(df_sensitive)
+            mean_df_sensitive = get_mean_sub_dataframe(df_sensitive)
+            mean_output_file_path_sensitive = os.path.join(
+                output_root_dir, "mean_results_sensitive.tex"
+            )
+            median_output_file_path_sensitive = os.path.join(
+                output_root_dir, "median_results_sensitive.tex"
+            )
+            Toolbox.create_folder(mean_output_file_path_sensitive)
+            Toolbox.create_folder(median_output_file_path_sensitive)
+            SingleTableGenerator.output(
+                mean_df_sensitive, mean_output_file_path_sensitive
+            )
+            SingleTableGenerator.output(
+                median_df_sensitive, median_output_file_path_sensitive
+            )
         case _:
             raise ValueError(f"Unknown mode: {mode}. Use 'mean', 'median' or 'all'.")
 
@@ -197,7 +241,7 @@ def main() -> None:
     """
     Main function to generate LaTeX tables for the results of the experiments.
     """
-    output_tex("median", ".")
+    output_tex("all", ".")
 
 
 if __name__ == "__main__":
