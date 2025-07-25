e54408085da010ca2ad5a933d3163f15ecd3dfa1
Congye Wang <congye.wang@outlook.com>
2025-07-25 21:46:34 +0100

    Bug fixed of filenames

--- experiments/run-generate_discount_factor.sh
+++ experiments/run-generate_discount_factor.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=run-generate_batch_discount_factor
+#SBATCH --job-name=run-generate_discount_factor
 #SBATCH --mail-type=FAIL
 
 module load GCC
 
 source /mnt/nfs/home/c2029946/Code/PythonProjects/pyrlmala/.venv/bin/activate
 
-python generate_batch_discount_factor.py
+python generate_discount_factor.py
 
 cd whole_results_discount_factor
 rm -rf test-*
 cd -
 
-bash submit_batch_discount_factor.sh
+bash submit_discount_factor.sh
 
 echo "Finished Job"
 exit 0
